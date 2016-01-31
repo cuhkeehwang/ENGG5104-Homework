13059e8e472af593c5b7230a57e67928d06f33d3
lizz <lizz@pku.edu.cn>
2016-02-01 00:11:31 +0800

    fix jpeg.

--- Assignment1/runJEPGcompress.py
+++ Assignment1/runJEPGcompress.py
@@ -12,48 +12,38 @@ def jpegCompress(image, quantmatrix):
     
     H = np.size(image, 0)
     W = np.size(image, 1)
-
-    # Number of 8x8 blocks in the height and width directions
-    h8 = int(H / 8)
-    w8 = int(W / 8)
     
-    # TODO If not an integer number of blocks, pad it with zeros
-    result = image.astype('float')
-    if h8 != np.ceil(h8) or w8 != np.ceil(w8):
-        h8, w8 = int(np.ceil((h8, w8)))
-        im = np.zeros((h8 * 8, w8 * 8, 3))
-        im[:H, :W, :] = result
-        result = im
+    # Number of 8x8 blocks in the height and width directions
+    h8, w8 = np.ceil((H / 8, W / 8)).astype('int')
 
-    # TODO Separate the result into blocks, and compress the blocks via quantization DCT coefficients
+    # TODO If not an integer number of blocks, pad it with zeros
+    result = np.pad(image.astype('float'), ((0, h8 * 8 - H), (0, w8 * 8 - W), (0, 0)), 'constant', constant_values=(0, 0))
+
+    # TODO Separate the result into blocks, and compress the blocks via
+    # quantization DCT coefficients
     for i in range(0, h8 * 8, 8):
         for j in range(0, w8 * 8, 8):
-            mat = np.round(np.divide(cv2.dct(result[i:i+8, j:j+8, 0] - 128), quantmatrix))
-            result[i:i+8, j:j+8, 0] = mat
-            result[i:i+8, j:j+8, 1] = mat
-            result[i:i+8, j:j+8, 2] = mat
+            mat = np.round(np.divide(cv2.dct(result[i:i + 8, j:j + 8, 0] - 128), quantmatrix))
+            result[i:i+8, j:j+8, :] = np.reshape(mat,(8,8,1))
 
     # TODO Convert back from DCT domain to RGB result
     for i in range(0, h8 * 8, 8):
         for j in range(0, w8 * 8, 8):
-            mat = cv2.idct(np.multiply(result[i:i+8, j:j+8, 0], quantmatrix)) + 128
-            result[i:i+8, j:j+8, 0] = mat
-            result[i:i+8, j:j+8, 1] = mat
-            result[i:i+8, j:j+8, 2] = mat
+            mat = cv2.idct(np.multiply(result[i:i + 8, j:j + 8, 0], quantmatrix)) + 128
+            result[i:i+8, j:j+8, :] = np.reshape(mat,(8,8,1))
     
-    return result.astype('uint8')
+    return result[:H, :W, :].astype('uint8')
 
 if __name__ == '__main__':
 
     im = cv2.imread('./misc/lena_gray.bmp')
-    im.astype('float')
     
     quantmatrix = sio.loadmat('./misc/quantmatrix.mat')['quantmatrix']
 
     out = jpegCompress(im, quantmatrix)
 
     # Show result, OpenCV is BGR-channel, matplotlib is RGB-channel
-    # Or: 
+    # Or:
     #   cv2.imshow('output',out)
     #   cv2.waitKey(0)
     plt.imshow(out)
